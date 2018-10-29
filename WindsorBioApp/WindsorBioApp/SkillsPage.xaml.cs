2d4a0abaa107cfc9d46d2da184fb554f1c1acc20
0766872 <dawindsor@gus.pittstate.edu>
2018-10-29 18:03:47 -0500

    Made enhanced list view.

--- WindsorBioApp/WindsorBioApp/SkillsPage.xaml.cs
+++ WindsorBioApp/WindsorBioApp/SkillsPage.xaml.cs
@@ -13,21 +13,22 @@ namespace WindsorBioApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkillsPage : ContentPage
 	{
-        public ObservableCollection<string> Skills { get; set; }
+        public ObservableCollection<Skill> Skills { get; set; }
 		public SkillsPage ()
 		{
 			InitializeComponent ();
 
-            this.Skills = new ObservableCollection<string>();
+            this.Skills = new ObservableCollection<Skill>();
+            this.Skills.Add(new Skill {Name = "Stuff", Description = "I like stuff"});
+            this.Skills.Add(new Skill {Name = "Things", Description = "I also like things"});
+            this.Skills.Add(new Skill {Name = "Rocket League", Description = "I got diamond clout"});
+            SkillList.ItemsSource = this.Skills;
 		}
 
-        private void SkillButton_Clicked(object sender, EventArgs e)
+        public class Skill
         {
-            this.Skills.Add(SkillEntry.Text);
-
-            SkillEntry.Text = "";
-
-            SkillList.ItemsSource = this.Skills;
-        }
+            public string Icon { get; set; }
+            public string Description { get; set; }
+            public string Name { get; set; }
     }
 }
